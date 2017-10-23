dec967d45816beaf88a357bf4584f41514414139
Daniel <sdcole96@yahoo.com>
2017-10-23 13:50:31 -0700

    EXCEPT FOR MY GRANDFATHER

---
+++ So You Think You Can Lance/Assets/treeTeleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeTeleport : MonoBehaviour {
+
+	public GameObject g;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		Debug.Log ("TELEPORT");
+		c.gameObject.transform.position = g.transform.position;
+	}
+}
