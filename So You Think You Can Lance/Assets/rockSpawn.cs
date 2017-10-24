f831a18fe3aa2f25a0bbdc03ced32897b1d82f2c
Daniel <sdcole96@yahoo.com>
2017-10-24 14:08:21 -0700

    How are those swipes coming along because these levels are a nice B-

---
+++ So You Think You Can Lance/Assets/rockSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockSpawn : MonoBehaviour {
+
+	public GameObject g;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		g.AddComponent<Rigidbody2D> ();
+	}
+}
