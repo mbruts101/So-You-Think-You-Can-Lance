11fd78a37e95700365407d2d0d6d662bdd338f67
Daniel <sdcole96@yahoo.com>
2017-10-25 22:03:50 -0700

    Updated ReadME

---
+++ So You Think You Can Lance/Assets/Daniel Scripts/rockSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockSpawn : MonoBehaviour {
+
+//Daniel Cole
+//1870787
+//cole149@mail.chapman.edu
+//Level Design II 344
+//So You Think You Can Lance
+//
+
+	public GameObject g;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		g.AddComponent<Rigidbody2D> ();
+	}
+}
