29fa0dea2a9bbafe149f75acdf3b1a44b593e756
Daniel <sdcole96@yahoo.com>
2017-11-14 17:34:10 -0800

    Added rolling rocks scripts

---
+++ So You Think You Can Lance/Assets/rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z+10));
+	}
+}
---
+++ So You Think You Can Lance/Assets/runLeft.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runLeft : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector2 (this.transform.position.x - speed, this.transform.position.y);
+	}
+}
