f6e7d16541b3cbe033d16ef63447e6abbf54dfb9
mbruts101 <bruts101@mail.chapman.edu>
2017-11-22 16:10:58 -0800

    Added Boss Fight Scene

---
+++ So You Think You Can Lance/Assets/Our Assets/Scripts/UserControlBoss.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySampleAssets._2D;
+using UnitySampleAssets.CrossPlatformInput;
+
+public class UserControlBoss : MonoBehaviour
+{
+  
+    private PlatformerCharacter2D character;
+    private bool jump;
+
+    private void Awake()
+    {
+        character = GetComponent<PlatformerCharacter2D>();
+    }
+
+    private void Update()
+    {
+        if (!jump)
+            // Read the jump input in Update so button presses aren't missed.
+            jump = CrossPlatformInputManager.GetButtonDown("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        //bool crouch = Input.GetKey(KeyCode.LeftControl);
+        //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        jump = false;
+    }
+}
