dda053658429e1e5259d4842dfe6d9887b5c2f92
Daniel <sdcole96@yahoo.com>
2017-11-27 14:36:01 -0800

    Stops/freezes player + Flips bandit for cutscene

---
+++ So You Think You Can Lance/Assets/cutScene1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutScene1 : MonoBehaviour {
+	public GameObject lance; 
+	public GameObject bandit2;
+	// Use this for initialization
+	void Start () {
+		bandit2 = GameObject.Find ("Bandit2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			col.gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionX;
+
+			col.gameObject.GetComponent<Animator> ().enabled = false;
+
+			Debug.Log ("CUT SCENE");
+			bandit2.GetComponent<SpriteRenderer> ().flipX = true;
+		}
+	}
+}
