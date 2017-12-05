7658d6cc95e65577d64b72e324fefffe0e9026a7
mbruts101 <bruts101@mail.chapman.edu>
2017-12-05 13:57:27 -0800

    Boss fight, scrolling script

---
+++ So You Think You Can Lance/Assets/Our Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,18 @@
+﻿//https://www.youtube.com/watch?v=QkisHNmcK7Y
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+    public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        this.GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
