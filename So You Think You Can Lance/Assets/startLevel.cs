d33337abc18d5713b59128cd280e7d00ddea90b0
Daniel <sdcole96@yahoo.com>
2017-12-09 21:00:35 -0800

    Added Fade to second level

---
+++ So You Think You Can Lance/Assets/startLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startLevel : MonoBehaviour {
+
+	public GameObject center;
+
+	void Start () 
+	{
+		center.GetComponent<Image> ().CrossFadeAlpha (0f, 5f, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
