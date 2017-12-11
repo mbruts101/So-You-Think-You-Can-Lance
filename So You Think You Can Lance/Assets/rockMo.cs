fd59d0938bb03b926eb2b392d956fcb48fdfe349
Daniel <sdcole96@yahoo.com>
2017-12-10 21:49:53 -0800

    this game is consuming me

---
+++ So You Think You Can Lance/Assets/rockMo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockMo : MonoBehaviour {
+	public float speed;
+	public int i = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position = new Vector3 (this.gameObject.transform.position.x - speed, this.gameObject.transform.position.y, 0f);
+		if (i == 0)
+		{
+			StartCoroutine (spin ());
+		}
+	}
+
+	IEnumerator spin()
+	{
+		while (i != 10000) 
+		{
+			yield return new WaitForSeconds (.001f);
+			this.transform.rotation = Quaternion.Euler (0f, 0f, i*2);
+			i++;
+		}
+
+	}
+
+
+}
---
+++ So You Think You Can Lance/Assets/waterDeath.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class waterDeath : MonoBehaviour {
+	
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GameObject.Find ("SirLance").GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			StartCoroutine (sink ());
+		}
+	}
+
+	IEnumerator sink()
+	{
+		yield return new WaitForSeconds (.2f);
+		rb.constraints = RigidbodyConstraints2D.FreezePosition;
+
+		GameObject g = GameObject.Find ("SirLance");
+		for (int i = 0; i <= 41; i++) 
+		{
+			yield return new WaitForSeconds (.00001f);
+			g.transform.rotation = Quaternion.Euler (0, 0, i);
+			if (i > 40)
+			{
+				rb.constraints = RigidbodyConstraints2D.FreezePositionX|RigidbodyConstraints2D.FreezeRotation;
+			}
+		}
+		yield return new WaitForSeconds (.4f);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
