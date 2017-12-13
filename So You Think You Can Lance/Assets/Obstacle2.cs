964f7d64de03a78f7733d125bf98c327eacd34c0
Daniel <sdcole96@yahoo.com>
2017-12-12 18:47:13 -0800

    skirt

---
+++ So You Think You Can Lance/Assets/Obstacle2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacle2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Player") 
+		{
+			Debug.Log (col.gameObject.name);
+			Destroy (col.gameObject);
+			SceneManager.LoadScene("level1");
+		}
+
+	}
+}
